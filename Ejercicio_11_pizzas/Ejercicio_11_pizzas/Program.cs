7dcd0d01531749a41946e9b7145a7c49674d273b
YEISON DAVID HENAO PAREJA <yeisonhenao.p@hotmail.com>
2024-09-13 10:52:29 -0500

    final

--- Ejercicio_11_pizzas/Ejercicio_11_pizzas/Program.cs
+++ Ejercicio_11_pizzas/Ejercicio_11_pizzas/Program.cs
@@ -12,14 +12,15 @@
         //mozzarella y el tomate que están en todas las pizzas.Al final se debe mostrar por pantalla
         //si la pizza elegida es vegetariana o no y todos los ingredientes que lleva.
         int NumEleccion, ingredienteVegano, ingredienteNormal;
-        string ingV,ingN1,ingN2,ingN3;
-        Console.Write("¡Bienvenido a la pizzeria Bella Napoli!\n" +
-            "Primero elige una de las siguientes opciones:\n" +
-            "1 si quieres una Pizza Vegetariana\n2 si quieres una Pizza Normal\nIngresa aquí tu eleccion:");
+        string ingV,ingN;
+        string ingredientesFijos = "Mozzarella, Tomate";
+        Console.Write("¡Bienvenido a la pizzeria Bella Napoli!\n\n" +
+            "Primero elige una de las siguientes opciones:\n\n" +
+            "1 si quieres una Pizza Vegetariana\n2 si quieres una Pizza Normal\n\nIngresa aquí tu eleccion:");
         NumEleccion=Convert.ToInt32(Console.ReadLine());
         if (NumEleccion == 1)
         {
-            Console.Write("Ahora Elige un ingrediente:\n1 Pimiento\n2 Tofu");
+            Console.Write("\nAhora Elige un ingrediente:\n1 Pimiento\n2 Tofu\n\nIngresa aquí tu eleccion:");
             ingredienteVegano = Convert.ToInt32(Console.ReadLine());
             if (ingredienteVegano == 1)
             {
@@ -29,12 +30,25 @@
             {
                 ingV = "Tofu";
             }
-            }
+            Console.Write($"\nTu pizza queda así:\n{ingredientesFijos} y {ingV}\n\n\n\n\n");
         }
         else if (NumEleccion == 2)
         {
-            Console.Write("Ahora Elige un ingrediente:\n1 Peperoni\n2 Jamón\n3 Salmón");
+            Console.Write("\nAhora Elige un ingrediente:\n1 Peperoni\n2 Jamón\n3 Salmón\n\nIngresa aquí tu eleccion:");
             ingredienteNormal = Convert.ToInt32(Console.ReadLine());
+            if (ingredienteNormal == 1)
+            {
+                ingN = "Peperoni";
+            }
+            else if (ingredienteNormal == 2)
+            {
+                ingN = "Jamón";
+            }
+            else
+            {
+                ingN = "Salmón";
+            }
+            Console.Write($"\nTu pizza queda así:\n{ingredientesFijos} y {ingN}\n\n\n\n\n");    
         }
         }
 }
