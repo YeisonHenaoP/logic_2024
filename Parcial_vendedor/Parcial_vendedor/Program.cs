52c317dacd590fe9fee60141f72354b01dd3f431
YEISON DAVID HENAO PAREJA <yeisonhenao.p@hotmail.com>
2024-09-13 20:22:25 -0500

    inicio

---
+++ Parcial_vendedor/Parcial_vendedor/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        decimal sueldoBase, venta1, venta2, venta3;
+        Console.WriteLine("Hello, World!");
+    }
+}
