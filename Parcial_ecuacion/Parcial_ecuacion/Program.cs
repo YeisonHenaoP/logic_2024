aac46305328baf796e9050d8abcbabe84ab1c793
YEISON DAVID HENAO PAREJA <yeisonhenao.p@hotmail.com>
2024-09-13 21:56:33 -0500

    guardar

---
+++ Parcial_ecuacion/Parcial_ecuacion/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        double a, b, x;   
+        Console.Write("Ingrese los coeficientes de la ecuacion\na: ");
+        a=Convert.ToDouble(Console.ReadLine());
+        Console.Write("\nb: ");
+        b = Convert.ToDouble(Console.ReadLine());
+        x = (-b / a);
+        if (a == 0)
+        {
+            Console.Write("\nLa ecuacion no tiene solucion");
+        }
+        
+    }
+}
