3e284ceacf86e03ff81fe3095e3ad144e9666f67
YEISON DAVID HENAO PAREJA <yeisonhenao.p@hotmail.com>
2024-10-07 21:31:05 -0500

    Estudiantes1

--- Evaluacioin_Estudiantes/Evaluacioin_Estudiantes/Program.cs
+++ Evaluacioin_Estudiantes/Evaluacioin_Estudiantes/Program.cs
@@ -2,6 +2,23 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("inicio");
-    }
-}
+        bool continuar = true;
+
+        while (continuar)
+        {
+            Console.Clear();
+            Console.WriteLine("sistema de Evaluación de Notas de Estudiantes");
+            int estudiantesGanaron = 0;
+            int estudiantesPerdieron = 0;
+            Console.Write("Ingrese la cantidad de estudiantes: ");
+            int cantidadEstudiantes = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= cantidadEstudiantes; i++)
+            {
+                Console.WriteLine($"\nEstudiante {i}:");
+                Console.Write("Ingrese la cantidad de notas del estudiante: ");
+                int cantidadNotas = int.Parse(Console.ReadLine());
+                double sumaNotas = 0;
+                
+
+            }
+        }
