0ea91d268c202355b91ac7318b9131623fd578a3
YEISON DAVID HENAO PAREJA <yeisonhenao.p@hotmail.com>
2024-10-07 21:00:28 -0500

    juego3

--- Juego_Adivinar/Juego_Adivinar/Program.cs
+++ Juego_Adivinar/Juego_Adivinar/Program.cs
@@ -2,17 +2,29 @@
 {
     private static void Main(string[] args)
     {
-        const int Naleatorio = 8;
-        int numjug;
-        bool bandera;
-        bandera = true;
-        Console.WriteLine("Cuantas personas desea participar entre 2 y 4? :");
-        numjug=Convert.ToInt32(Console.ReadLine());
-        while(bandera)
+        bool jugarDeNuevo = true;
+        while (jugarDeNuevo)
         {
-
+            Console.Clear();
+            Console.WriteLine("¡bienvenidos al juego 'Adivina el número'!");
+            int numJugadores = 0;
+            while (numJugadores < 2 || numJugadores > 4)
+            {
+                Console.Write("ingrese la cantidad de jugadores (entre 2 y 4): ");
+                numJugadores = int.Parse(Console.ReadLine());
+            }
+            int rangoMax = 0;
+            if (numJugadores == 2) rangoMax = 50;
+            if (numJugadores == 3) rangoMax = 100;
+            if (numJugadores == 4) rangoMax = 200;
+            Random random = new Random();
+            int numeroSecreto = random.Next(0, rangoMax + 1);
+            Console.WriteLine($"Se ha generado un número secreto entre 0 y {rangoMax}. ¡Que comience el juego!");
+            bool numeroAdivinado = false;
+            int turno = 0;
+           
         }
 
-      
+
     }
 }
